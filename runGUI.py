4a010054c033c2d82828b88f24899679f61982db
Niki Stavropoulou <st_niki@hotmail.com>
2017-08-14 21:52:38 +0100

    Delete runGUI.py

--- runGUI.py
+++
@@ -1,8 +0,0 @@
-from Classes import *
-from Rules import rules
-
-root = Tk()
-root.resizable(width=False, height=False)
-gui = GUI(root)
-root.mainloop() #runs until quit or closed
-root.destroy() #cannot invoke, application is destroyed
